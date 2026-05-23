233cbb90167f2c4dd0bd2b4551dfed83b26050f8
troydenpihema <troydenpihema@gmail.com>
2026-05-24 11:18:14 +1200

    snake game addded

--- SnakeGame/obj/Debug/net10.0/SnakeGame.AssemblyInfo.cs
+++ SnakeGame/obj/Debug/net10.0/SnakeGame.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SnakeGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76e6fb0a309d27f6ab8ecfc3847b49be0be11b77")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7dbd7accae6a6b57a815df59d019ac46b70003c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("SnakeGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SnakeGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
