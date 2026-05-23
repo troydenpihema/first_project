8cc7d84888cf296ae9ebfc0baeed0516f1d6c48b
troydenpihema <troydenpihema@gmail.com>
2026-05-23 19:21:33 +1200

    new levels

--- PlatformGame/obj/Debug/net10.0/PlatformGame.AssemblyInfo.cs
+++ PlatformGame/obj/Debug/net10.0/PlatformGame.AssemblyInfo.cs
@@ -13,11 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlatformGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-<<<<<<< HEAD
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c20fc2178ca7b6e65da3fbbadce2140d64bd6c8")]
-=======
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2cf08d00f5aa1a96e155aaad173d7bae8beaec0")]
->>>>>>> cb7069b9e6632face8ea5eeb0c57fbb60653d42b
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20df111736ce326eae477e8b02ff1d58e4fc9df1")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlatformGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlatformGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
